5d3b88aab3c609ef69bd507f34946fd8afa67c87
Alejandro Perez Malagon <alepmalagon@gmail.com>
2020-08-24 10:57:28 -0400

    222

--- experiments/1000/process.m
+++ experiments/1000/process.m
@@ -1,4 +1,8 @@
-function[c, T3] = process(file_name, file_name_2, samples)
+function[c, c_2] = process(file_name, file_name_2, samples, kBps, payload)
+    time = samples*payload/(1000*kBps);
+    interval = payload/(1000*kBps);
+    x_vector = 0:interval:2*time;
+    x_vector = x_vector';
     T = csvread(file_name, 1, 0);
     T_2 = csvread(file_name_2, 1, 0);
     [m,n] = size(T);
@@ -10,18 +14,18 @@ function[c, T3] = process(file_name, file_name_2, samples)
     c = setdiff(0:(samples-1), T3);
     c_2 = setdiff(0:(samples-1), T_23);
     [m1,n1] = size(c);
-    subplot(4,1,1), plot(T(:,1), T(:,2)/1000);
+    subplot(4,1,1), plot(T(:,1)*interval, T(:,2)/1000);
+    title(strrep(strcat(file_name, file_name_2),'_',' '));
     grid;
-    subplot(4,1,2), plot(T_2(:,1), T_2(:,2)/1000);
+    subplot(4,1,2), plot(T_2(:,1)*interval, T_2(:,2)/1000);
     grid;
-    subplot(4,1,3), plot(consecutive(T, samples));
+    sss = size(consecutive(T, samples));
+    sss2 = size(consecutive(T_2, samples));
+
+    subplot(4,1,3), plot(x_vector(1:sss(2)),consecutive(T, samples));
     grid;
-    axis([0 samples 0 samples/500]);
-    axis 'auto y';
-    subplot(4,1,4), plot(consecutive(T_2, samples));
+    subplot(4,1,4), plot(x_vector(1:sss2(2)),consecutive(T_2, samples));
     grid;
-    axis([0 samples 0 samples/500]);
-    axis 'auto y';
     saveas(gcf,strcat(strcat(file_name, file_name_2),'.png'));
 end
 
