54e0336e2729b8713e421ba102f16a76079a49a1
Alejandro Perez Malagon <alepmalagon@gmail.com>
2020-08-24 12:54:58 -0400

    hhhh

--- roundtripmeasurement.py
+++ roundtripmeasurement.py
@@ -68,7 +68,7 @@ latencies of each packet received back from the server."""
                     break
                 send_addr = (recv_addr[0], listen_port)
                 sock_out.sendto(data, send_addr)
-                print (".") 
+                print (str(send_addr)) 
             except KeyboardInterrupt:
                 break
         print("Closing...")
