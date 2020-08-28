6442d8be4918b688301f1be59edf039e19b56994
Alejandro Perez Malagon <alepmalagon@gmail.com>
2020-08-28 11:17:06 -0400

    aaa

--- roundtripmeasurement.py
+++ roundtripmeasurement.py
@@ -71,7 +71,7 @@ latencies of each packet received back from the server."""
         while True:
             try:
                 data, recv_addr = sock_in.recvfrom(recv_buffer_size)
-                conn.send(str(recv_addr[1]).zfill(10).decode('ascii'))
+                conn.send(str(recv_addr[1]).zfill(10).encode('ascii'))
                 if not data:
                     break
                 send_addr = (recv_addr[0], int(recv_addr[1]))
