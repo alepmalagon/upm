532df2b09c8005d086e09ca71ae8f318822dd8a9
Alejandro Perez Malagon <alepmalagon@gmail.com>
2020-08-28 15:03:24 -0400

    aaaa

--- measurement.py
+++ measurement.py
@@ -27,7 +27,7 @@ class Measurement:
         sock_out = \
             socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         print (device)
-        if (device!='server'):
+        if (device!='serv'):
             sock_out.setsockopt (
                     socket.SOL_SOCKET,
                     25,
